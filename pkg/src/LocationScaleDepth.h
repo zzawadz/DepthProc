06ddee738376a418dd175be090cf35c097d57662
zzawadz <zawadzkizygmunt@gmail.com>
2014-06-01 14:17:59 +0200

    Added wrapper for sampleMaxLocScaleDepth

--- pkg/src/LocationScaleDepth.h
+++ pkg/src/LocationScaleDepth.h
@@ -12,6 +12,7 @@ namespace LSD
 {
   arma::vec sampleDepthContForMu(size_t d, double mu, const arma::vec& y, size_t m);
   arma::vec sampleMaxDepthForMu(double mu,const arma::vec& y, size_t d_min, size_t max_iter, double eps);
+  arma::vec sampleMaxLocScaleDepth(arma::vec y, size_t max_iter, double eps, double p_length);
 }
 
 
