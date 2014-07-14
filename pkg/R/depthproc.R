e724a6d9dad960cf4352962f587ab71b10dee2b8
zzawadz <zawadzkizygmunt@gmail.com>
2014-07-14 19:42:28 +0200

    Data update

--- pkg/R/depthproc.R
+++ pkg/R/depthproc.R
@@ -1,6 +1,5 @@
 #' US Labour dataset 
 #'
-#'
 #' @docType data
 #' @keywords datasets
 #' @name USLABOUR
@@ -8,3 +7,32 @@
 #' @format A data frame with 654 rows and 4 variables
 #' @source U.S.Department of Labor - Bureau of Labour Statistics FRED
 NULL
+
+
+#' Children under 5 months mortality rate per 1,000 live births
+#' @docType data
+#' @keywords datasets
+#' @name under5.mort
+#' @usage data(under5.mort)
+#' @format A data frame with 654 rows and 4 variables
+#' @source http://mdgs.un.org/unsd/mdg/Data.aspx
+NULL
+
+#' Infant mortality rate (0-1 year) per 1,000 live births
+#' @docType data
+#' @keywords datasets
+#' @name inf.mort
+#' @usage data(inf.mort)
+#' @format A data frame with 654 rows and 4 variables
+#' @source http://mdgs.un.org/unsd/mdg/Data.aspx
+NULL
+
+#' Children 1 year old immunized against measles, percentage
+#' @docType data
+#' @keywords datasets
+#' @name maesles.imm
+#' @usage data(maesles.imm)
+#' @format A data frame with 654 rows and 4 variables
+#' @source http://mdgs.un.org/unsd/mdg/Data.aspx
+NULL
+
