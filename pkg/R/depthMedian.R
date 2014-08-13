6acfcd6f6d9483877364da3b1c9d1ea0599ca783
Zygmunt Zawadzki <zzawadzki@zzawadzki-ThinkCentre-A58.(none)>
2014-08-13 11:32:22 +0200

    depthMedian update

--- pkg/R/depthMedian.R
+++ pkg/R/depthMedian.R
@@ -1,11 +1,23 @@
 #' @title Depth median
 #' @export
-depthMedian = function(x,...)
+#' 
+
+?setMethod
+setGeneric("depthMedian", function(x,...) standardGeneric("depthMedian"))
+
+setMethod("depthMedian", "matrix", function(x,...)
 {
   depths = depth(x,x,...)
   med = x[depths == max(depths),]
   if(ncol(x) != length(med)) med = colMeans(med)
   med
-}
+})
 
+setMethod("depthMedian", "Depth", function(x)
+{
+  pos = which(x== max(x))
+  med = x@u[pos,]
+  if(ncol(x@u) != length(med)) med = colMeans(med)
+  med  
+})
 
