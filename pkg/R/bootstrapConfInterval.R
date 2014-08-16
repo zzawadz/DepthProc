4ba9d5cacea7355cf5f1b15120417ede7e7bbe4e
zzawadz <zawadzkizygmunt@gmail.com>
2014-08-16 15:59:09 +0200

    Remove bootstrap

--- pkg/R/bootstrapConfInterval.R
+++
@@ -1,9 +0,0 @@
-#' @title Bootstrap confidence interval for depth median
-#' 
-#' @importFrom boot boot
-#'
-medianDepthConfInterval = function(x, method="Projection", R = 1000, ...)
-{ 
-  depthTMP = function(x, indices, ...) depthMedian(x=x[indices,],...)  
-  boot(x,depthTMP, R = R, method = method,...)
-}
