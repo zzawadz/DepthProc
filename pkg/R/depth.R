a5b1b4fc22768a25946666c2163c8e12d96e4d5f
zzawadz <zawadzkizygmunt@gmail.com>
2014-10-04 14:08:28 +0200

    Rmd update

--- pkg/R/depth.R
+++ pkg/R/depth.R
@@ -357,6 +357,9 @@ depthLP = function(u, X, pdim = 1, la = 1, lb = 1, name = "X", threads = -1, fun
 #'@export
 #'@description Computes the modified bannd depth.
 #'
+#' @param u Numerical vector or matrix whose depth is to be calculated. Dimension has to be the same as that of the observations.
+#' @param X The data as a matrix, data frame or list. If it is a matrix or data frame, then each row is viewed as one multivariate observation. If it is a list, all components must be numerical vectors of equal length (coordinates of observations).
+#'
 #'@examples
 #'
 #'  x = matrix(rnorm(3000), nc = 2)
