d30d478fa3dc9fbb6b6864be3cdca030b444e614
krzyslom <krzysztofslomczynski@gmail.com>
2017-03-30 21:33:35 +0200

    Style and args in ddmvnorm.R

--- pkg/R/ddmvnorm.R
+++ pkg/R/ddmvnorm.R
@@ -10,7 +10,7 @@
 #' @param ... Parameters passed to \code{depth}
 #' 
 #' @description
-#' Produces a  normal DD plot of a multivirate dataset.
+#' Produces a normal DD plot of a multivirate dataset.
 #'
 #' @details
 #' 
@@ -19,46 +19,44 @@
 #' @return
 #' Returns the normal depth versus depth plot of multivariate dataset \code{x}. 
 #'
-#'
 #' @references
 #' 
-#' Liu, R.Y., Parelius, J.M. and Singh, K. (1999), Multivariate analysis by data depth: Descriptive statistics, graphics and inference (with discussion),  Ann. Statist.,  27, 783-858.
+#' Liu, R.Y., Parelius, J.M. and Singh, K. (1999), Multivariate analysis by data depth: Descriptive statistics, graphics and inference (with discussion), Ann. Statist., 27, 783-858.
 #' 
 #' Liu, R.Y., Singh K. (1993), A Quality Index Based on Data Depth and Multivariate Rank Test, \emph{Journal of the American Statistical Association} vol. 88.
 #'
 #' @author Daniel Kosiorowski, Mateusz Bocian, Anna Wegrzynkiewicz and Zygmunt Zawadzki from Cracow University of Economics.
-#'  
+#' 
 #' @seealso \code{\link{ddPlot}} to generate ddPlot to compare to datasets or to compare a dataset with other distributions.
 #'
 #' @examples
 #' # EXAMPLE 1
-#' norm = mvrnorm(1000, c(0,0,0), diag(3))
-#' con = mvrnorm(100, c(1,2,5), 3*diag(3))
-#' sample = rbind(norm, con)
-#' ddMvnorm(sample, robust=TRUE)
+#' norm <- mvrnorm(1000, c(0, 0, 0), diag(3))
+#' con <- mvrnorm(100, c(1, 2, 5), 3 * diag(3))
+#' sample <- rbind(norm, con)
+#' ddMvnorm(sample, robust = TRUE)
 #' 
 #' # EXAMPLE 2
-#' data(under5.mort,inf.mort,maesles.imm)
-#' data1990=na.omit(cbind(under5.mort[,1],inf.mort[,1],maesles.imm[,1]))
-#' ddMvnorm(data1990, robust=FALSE)
+#' data(under5.mort, inf.mort, maesles.imm)
+#' data1990 <- na.omit(cbind(under5.mort[, 1], inf.mort[, 1], maesles.imm[, 1]))
+#' ddMvnorm(data1990, robust = FALSE)
 #' 
-ddMvnorm <-function(x, size = nrow(x), robust=FALSE, alpha=0.05, title = "ddMvnorm", ...)     
-{
-  depth_sample <- depth(x, x,  ...)  
+ddMvnorm <- function(x, size = nrow(x), robust = FALSE, alpha = 0.05,
+                     title = "ddMvnorm", ...) {
+  depth_sample <- depth(x, x, ...)
   
-  if(robust == TRUE) 
-  {
-  	varcov <- cov(x[depth_sample>=quantile(depth_sample, alpha),])
-  	location <- depthMedian(x, ...)
-  } 
-  else
-  { 
-  	location <- apply(x, 2, mean)
-  	varcov  <- cov(x) 
+  if (robust) {
+    varcov <- cov(x[depth_sample >= quantile(depth_sample, alpha), ])
+    location <- depthMedian(x, ...)
+  } else {
+    location <- apply(x, 2, mean)
+    varcov <- cov(x)
   }
-  theoretical <- mvrnorm(size, location, varcov)  
+  
+  theoretical <- mvrnorm(size, location, varcov)
   depth_theoretical <- depth(x, theoretical, ...)
-  ddplot = new("DDPlot",X = depth_sample, Y = depth_theoretical, title = title)
+  ddplot <- new("DDPlot", X = depth_sample, Y = depth_theoretical,
+                title = title)
   
   return(ddplot)
 }
