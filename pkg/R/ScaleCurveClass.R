028f5cee2ebeaf7ddbef5650e5b4bda1239cffe3
krzyslom <krzysztofslomczynski@gmail.com>
2017-04-01 01:02:56 +0200

    Style and args in ScaleCurveClass.R

--- pkg/R/ScaleCurveClass.R
+++ pkg/R/ScaleCurveClass.R
@@ -1,13 +1,10 @@
 #' @rdname getPlot-methods
 #' @aliases getPlot,ScaleCurveList
 #' @export
-setMethod("getPlot", "ScaleCurveList", function(object)
-{
-  p = .getPlot(object)
-  p = p + ggtitle(object[[1]]@title)
-  p = p + ylab("Volume")
-  p = p + xlab("Alpha")
+setMethod("getPlot", "ScaleCurveList", function(object) {
+  p <- .getPlot(object)
+  p <- p + ggtitle(object[[1]]@title)
+  p <- p + ylab("Volume")
+  p <- p + xlab("1 - alpha")
   return(p)
 })
-
-
