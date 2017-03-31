e76f238522468da5397b0f542f1963f015f2a91b
krzyslom <krzysztofslomczynski@gmail.com>
2017-03-31 19:37:12 +0200

    Style and args in fncUtils.R

--- pkg/R/fncUtils.R
+++ pkg/R/fncUtils.R
@@ -5,16 +5,17 @@
 #' If matrix has column names, then oredered factor is returned.
 #' 
 #' @examples
-#' u = matrix(1:10, ncol = 2)
+#' u <- matrix(1:10, ncol = 2)
 #' .extractIndexFromMatrix(u)
-#' colnames(u) = c("fn1", "fn2")
+#' colnames(u) <- c("fn1", "fn2")
 #' .extractIndexFromMatrix(u)
 #' 
-.extractIndexFromMatrix = function(x)
-{
-  if( is.null(colnames(x)) ) return(1:ncol(x))
+.extractIndexFromMatrix <- function(x) {
   
-  idx = colnames(x)
+  if (is.null(colnames(x))) {
+    return(1:ncol(x))
+  }
+  
+  idx <- colnames(x)
   factor(idx, ordered = TRUE)
 }
-
