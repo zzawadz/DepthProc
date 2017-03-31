78a0634b15684d6d0fa92e31c08de0a833549b35
krzyslom <krzysztofslomczynski@gmail.com>
2017-03-31 17:38:54 +0200

    Style and args in depthMedian.R

--- pkg/R/depthMedian.R
+++ pkg/R/depthMedian.R
@@ -14,43 +14,49 @@
 #' @examples
 #' 
 #' # depthMedian for matrix
-#' x = matrix(rnorm(600), nc = 3)
+#' x <- matrix(rnorm(600), nc = 3)
 #' depthMedian(x)
 #' 
 #' # depthMedian works with object of class Depth
-#' dp = depth(x)
+#' dp <- depth(x)
 #' depthMedian(dp) 
 #' 
-setGeneric("depthMedian", function(x,...) standardGeneric("depthMedian"))
+setGeneric("depthMedian", function(x, ...) {
+  standardGeneric("depthMedian")
+})
 
 #' @rdname depthMedian-methods
-#' @aliases depthMedian,matrix
+#' @aliases depthMedian matrix
 #' @export
-setMethod("depthMedian", "matrix", function(x,...)
-{
-  depths = depth(x,x,...)
-  med = x[depths == max(depths),]
-  if(ncol(x) != length(med)) med = colMeans(med)
+setMethod("depthMedian", "matrix", function(x, ...) {
+  depths <- depth(x, x, ...)
+  med <- x[depths == max(depths), ]
+  
+  if (ncol(x) != length(med)) {
+    med <- colMeans(med)
+  }
+  
   med
 })
 
 #' @rdname depthMedian-methods
-#' @aliases depthMedian,data.frame
+#' @aliases depthMedian data.frame
 #' @export
-setMethod("depthMedian", "data.frame", function(x,...)
-{
-  x = as.matrix(x)
+setMethod("depthMedian", "data.frame", function(x, ...) {
+  x <- as.matrix(x)
   depthMedian(x, ...)
 })
 
 #' @rdname depthMedian-methods
-#' @aliases depthMedian,Depth
+#' @aliases depthMedian Depth
 #' @export
-setMethod("depthMedian", "Depth", function(x)
-{
-  pos = which(x== max(x))
-  med = x@u[pos,]
-  if(ncol(x@u) != length(med)) med = colMeans(med)
-  med  
+setMethod("depthMedian", "Depth", function(x) {
+  pos <- which(x == max(x))
+  med <- x@u[pos, ]
+  
+  if (ncol(x@u) != length(med)) {
+    med <- colMeans(med)
+  }
+  
+  med
 })
-
