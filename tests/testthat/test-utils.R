ab1b6e76e58e21da7ef9fc83477730ef4f17541d
Zygmunt Zawadzki <zygmunt@zstat.pl>
2018-09-18 14:04:11 +0200

    test extractIndexFromMatrix

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -6,9 +6,15 @@ test_that("DepthProc Theme", {
   expect_doppelganger("depthproc theme", pp)
 })
 
+test_that("extractIndexFromMatrix", {
+  x <- cbind(1,1,1)
+  expect_equal(DepthProc:::extractIndexFromMatrix(x), 1:3)
+})
+
 test_that(".testNorm", {
   expect_equal(dim(DepthProc:::.testNorm(3)), c(100, 3))
 })
+
 
 test_that(".addAlpha", {
   expect_equivalent(DepthProc:::.addAlpha("red", 0.5),
